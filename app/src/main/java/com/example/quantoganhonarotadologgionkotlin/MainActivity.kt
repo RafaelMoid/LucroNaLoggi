94b804da30a73495bf89574f2fbba9405d4d92cf
Rafael Varela <rafaelvrcr@gmail.com>
2021-05-14 13:52:12 -0300

    I just started to develop this app to get to know the kotlin language, i want to make my journey delivering at loggi, hope it will help you too.

---
+++ app/src/main/java/com/example/quantoganhonarotadologgionkotlin/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.quantoganhonarotadologgionkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botaoCalcular = calcular
+        val resultado = resultado
+
+
+
+        //Criar o efeito de click no botão de calcular
+        botaoCalcular.setOnClickListener{
+
+        var valorKm = valorKm
+        var valorReal = valorReal
+        var valorLitro = valorLitro
+        val result = valorKm.
+            resultado.setText()
+
+        }
+
+
+    }
+}
